513b302025c17870c64c4877e486a4830ced3271
abraham reta <95265657+areta1921@users.noreply.github.com>
2022-06-22 13:31:37 -0400

    inital commit

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Nav.css'
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><span>Abr</span>aham</li>
+        <li>CRYPTO WORLD</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
